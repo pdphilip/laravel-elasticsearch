b81f155a5a0b029d7992249e91b07c7993218073
David Philip <pd.philip@gmail.com>
2023-05-27 02:34:06 +0200

    Elasticsearch 8.7+ X Laravel 6 upgrade

--- src/DSL/Results.php
+++ src/DSL/Results.php
@@ -5,21 +5,27 @@ namespace PDPhilip\Elasticsearch\DSL;
 class Results
 {
     private $_meta;
-
+    
     public $data;
     public $errorMessage;
-
-
+    
+    
     public function __construct($data, $meta, $params, $queryTag)
     {
         $this->data = $data;
+        
+        //ensure meta is an array and not an object
+        if (is_object($meta)) {
+            $meta = (array)$meta;
+        }
+        unset($meta['_source']);
         $this->_meta = ['query' => $queryTag] + $meta;
         $this->_meta['params'] = $params;
         $this->_meta['_id'] = $data['_id'] ?? null;
         $this->_meta['success'] = true;
-
+        
     }
-
+    
     public function setError($error, $errorCode)
     {
         if ($this->_isJson($error)) {
@@ -32,50 +38,50 @@ class Results
         $this->_meta['error']['code'] = $errorCode;
         $this->_meta['success'] = false;
         $this->errorMessage = $error;
-
+        
     }
-
+    
     public function isSuccessful()
     {
         return $this->_meta['success'] ?? false;
     }
-
+    
     public function getMetaData()
     {
         return $this->_meta;
     }
-
+    
     public function getLogFormattedMetaData()
     {
         $return = [];
         foreach ($this->_meta as $key => $value) {
             $return['logged_'.$key] = $value;
         }
-
+        
         return $return;
     }
-
+    
     public function getInsertedId()
     {
         return $this->_meta['_id'] ?? null;
     }
-
-
+    
+    
     public function getModifiedCount()
     {
         return $this->_meta['modified'] ?? 0;
     }
-
+    
     public function getDeletedCount()
     {
         return $this->_meta['deleted'] ?? 0;
     }
-
+    
     private function _isJson($string)
     {
         json_decode($string);
-
+        
         return (json_last_error() == JSON_ERROR_NONE);
     }
-
+    
 }
--- src/Eloquent/Docs/ModelDocs.php
+++ src/Eloquent/Docs/ModelDocs.php
@@ -1,16 +1,18 @@
 <?php
 
 namespace PDPhilip\Elasticsearch\Eloquent\Docs;
+
 /**
- * @method static $this term(string $term,$boostFactor = null) @return $this
- * @method $this andTerm(string $term,$boostFactor = null)
- * @method $this orTerm(string $term,$boostFactor = null)
- * @method static $this fuzzyTerm(string $term,$boostFactor = null)
- * @method $this andFuzzyTerm(string $term,$boostFactor = null)
- * @method $this orFuzzyTerm(string $term,$boostFactor = null)
- * @method static  $this regEx(string $term,$boostFactor = null)
- * @method $this andRegEx(string $term,$boostFactor = null)
- * @method $this orRegEx(string $term,$boostFactor = null)
+ * @method static $this term(string $term, $boostFactor = null) @return $this
+ * @method $this andTerm(string $term, $boostFactor = null)
+ * @method $this orTerm(string $term, $boostFactor = null)
+ * @method $this boostField(string $field, $boostFactor = null)
+ * @method static $this fuzzyTerm(string $term, $boostFactor = null)
+ * @method $this andFuzzyTerm(string $term, $boostFactor = null)
+ * @method $this orFuzzyTerm(string $term, $boostFactor = null)
+ * @method static $this regEx(string $term, $boostFactor = null)
+ * @method $this andRegEx(string $term, $boostFactor = null)
+ * @method $this orRegEx(string $term, $boostFactor = null)
  * @method $this minShouldMatch(int $value)
  * @method $this minScore(float $value)
  * @method $this field(string $field, int $boostFactor = null)
@@ -19,10 +21,11 @@ namespace PDPhilip\Elasticsearch\Eloquent\Docs;
  * @method query(array $columns = '*')
  *
  * @method  $this filterGeoBox(string $column, array $topLeftCoords, array $bottomRightCoords)
- * @method  $this filterGeoPoint(string $column,string $distance,array $point)
- * @method  $this whereRegex(string $column,string $regex)
+ * @method  $this filterGeoPoint(string $column, string $distance, array $point)
+ * @method  $this whereRegex(string $column, string $regex)
  *
  * @mixin \Illuminate\Database\Query\Builder
  */
-
-trait ModelDocs{}
+trait ModelDocs
+{
+}
