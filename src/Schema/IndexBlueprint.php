152b51d4aae116748a8afff2fc2604ab1eeed1a2
David Philip <pd.philip@gmail.com>
2024-05-15 12:10:09 +0200

    Added the remaining numeric fields

--- src/Schema/IndexBlueprint.php
+++ src/Schema/IndexBlueprint.php
@@ -53,30 +53,59 @@ class IndexBlueprint
         return $this->addField('keyword', $field);
     }
     
-    public function integer($field): Definitions\FieldDefinition
-    {
-        return $this->addField('integer', $field);
-    }
+    
+    //----------------------------------------------------------------------
+    // Numeric Types
+    //----------------------------------------------------------------------
     
     public function long($field): Definitions\FieldDefinition
     {
         return $this->addField('long', $field);
     }
     
-    public function float($field): Definitions\FieldDefinition
+    public function integer($field): Definitions\FieldDefinition
     {
-        return $this->addField('float', $field);
-    }
-
-    public function double($field): Definitions\FieldDefinition
-    {
-        return $this->addField('double', $field);
+        return $this->addField('integer', $field);
     }
     
     public function short($field): Definitions\FieldDefinition
     {
         return $this->addField('short', $field);
     }
+    
+    public function byte($field): Definitions\FieldDefinition
+    {
+        return $this->addField('byte', $field);
+    }
+    
+    public function double($field): Definitions\FieldDefinition
+    {
+        return $this->addField('double', $field);
+    }
+    
+    public function float($field): Definitions\FieldDefinition
+    {
+        return $this->addField('float', $field);
+    }
+    
+    public function halfFloat($field): Definitions\FieldDefinition
+    {
+        return $this->addField('half_float', $field);
+    }
+    
+    public function scaledFloat($field, $scalingFactor = 100): Definitions\FieldDefinition
+    {
+        return $this->addField('scaled_float', $field, [
+            'scaling_factor' => $scalingFactor,
+        ]);
+    }
+    
+    public function unsignedLong($field): Definitions\FieldDefinition
+    {
+        return $this->addField('unsigned_long', $field);
+    }
+    
+    //----------------------------------------------------------------------
     
     public function date($field, $format = null): Definitions\FieldDefinition
     {
