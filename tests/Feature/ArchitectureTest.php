98e19c3bc271af052983c628ec551e886a427baf
use-the-fork <sincore@gmail.com>
2024-08-01 18:13:25 -0400

    refactor(tests): relocate and enhance ArchitectureTest for debugging prevention

--- tests/Feature/ArchitectureTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-  declare(strict_types=1);
-
-  #saftey tests to make sure we never push up a package that can dump and die.
-  arch("doesn't use dd, dump, or var_dump")
-    ->expect(['dd', 'dump', 'var_dump'])
-    ->not->toBeUsed();
