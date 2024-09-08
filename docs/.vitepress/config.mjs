c4e13d90ab41cfc03cf9b48ca15039cb2dca4c37
use-the-fork <sincore@gmail.com>
2024-09-08 15:59:37 -0400

    docs(config): add lastUpdated feature in VitePress config
    - Enabled the 'lastUpdated' option in VitePress configuration.
    - Updated the project documentation metadata.
    
    Keeping things fresh and accurate! 🕒

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
   title: 'Laravel-Elasticsearch',
   description: "An Elasticsearch implementation of Laravel's Eloquent ORM",
+  lastUpdated: true,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     logo: '/logo.svg',
