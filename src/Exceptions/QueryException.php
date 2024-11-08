b88b16ce16ac4fbcfcd2d1f2784d84b95bca5ca2
use-the-fork <sincore@gmail.com>
2024-11-08 18:30:05 -0500

    fix(exceptions): enhance error message formatting and clean up comments
    - Refactor `BulkInsertQueryException` to remove commented-out code for streamlined maintenance.
    - Update `QueryException` constructor to utilize a new `formatMessage` method.
    - Add `formatMessage` and helper methods to construct detailed error messages for specific exception types.
    - Leverage `Illuminate\Support\Collection` for assembling error messages efficiently.
    
    Now your errors come with a touch of elegance! 🎩

--- src/Exceptions/QueryException.php
+++ src/Exceptions/QueryException.php
@@ -5,20 +5,45 @@ declare(strict_types=1);
 namespace PDPhilip\Elasticsearch\Exceptions;
 
 use Exception;
+use Illuminate\Support\Collection;
 
 class QueryException extends Exception
 {
     private array $_details;
 
-    public function __construct($message, $code = 0, ?Exception $previous = null, $details = [])
+    public function __construct($message, $code = 0, ?Exception $previous = null)
     {
-        parent::__construct($previous->getMessage(), $previous->code, $previous);
-
-        $this->_details = $details;
+      parent::__construct($this->formatMessage($previous), $previous->code);
     }
 
-    public function getDetails(): array
-    {
-        return $this->_details;
-    }
+
+  /**
+   * Format the error message.
+   *
+   * Takes the first {$this->errorLimit} bulk issues and concatenates them to a single string message
+   *
+   * @param  Exception  $result
+   * @return string
+   */
+  private function formatMessage(Exception $result): string
+  {
+    $error = json_decode((string) $result->getResponse()->getBody(), true);
+
+    // Clean that ish up.
+    return match ($error['error']['type']) {
+      'script_exception' => $this->formatScriptException($error),
+      'index_not_found_exception' => $error['error']['reason'] = 'Index does not exist',
+    };
+  }
+
+  private function formatScriptException($error): string
+  {
+    $message = collect();
+    $message->push("{$error['error']['type']}: {$error['error']['reason']}");
+    $message->push("Caused By: {$error['error']['caused_by']['type']}");
+    $message->push(...$error['error']['script_stack']);
+
+    return $message->implode(PHP_EOL);
+  }
+
 }
