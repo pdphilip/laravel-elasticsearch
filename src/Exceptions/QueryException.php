0a7b94befda706d3cd77148000c52ee37115c2d2
use-the-fork <sincore@gmail.com>
2024-11-27 20:09:10 -0500

    fix(exceptions): correct error handling in QueryException for better debugging
    - Changed error type access from `$error['type']` to `$error['error']['type']`.
    - Added a conditional check to include phase index if it's not empty.
    
    Now with 50% more accuracy and 100% less confusion! 🚀

--- src/Exceptions/QueryException.php
+++ src/Exceptions/QueryException.php
@@ -7,84 +7,81 @@ namespace PDPhilip\Elasticsearch\Exceptions;
 use Elastic\Elasticsearch\Exception\ClientResponseException;
 use Elastic\Elasticsearch\Exception\MissingParameterException;
 use Exception;
-use Illuminate\Support\Collection;
 
 class QueryException extends Exception
 {
     public function __construct(Exception $previous)
     {
-      parent::__construct($this->formatMessage($previous), $previous->code);
+        parent::__construct($this->formatMessage($previous), $previous->code);
     }
 
-
-  /**
-   * Format the error message.
-   *
-   * Takes the first {$this->errorLimit} bulk issues and concatenates them to a single string message
-   *
-   * @param  Exception  $result
-   * @return string
-   */
-  private function formatMessage(Exception $result): string
-  {
-    // Clean that ish up.
-    return match (get_class($result)) {
-      MissingParameterException::class => $this->formatMissingParameterException($result),
-      ClientResponseException::class => $this->routeClientResponseException($result),
-    };
-  }
-
-  private function formatMissingParameterException($result): string
-  {
-    return $result->getMessage();
-  }
-
-  private function routeClientResponseException($result): string
-  {
-
-    $error = json_decode((string) $result->getResponse()->getBody(), true);
-
-    return match ($error['error']['type']) {
-      'search_phase_execution_exception' => $this->formatSearchPhaseExecutionException($error),
-      'script_exception' => $this->formatScriptException($error),
-      default => $this->formatParseException($error),
-    };
-  }
-
-  private function formatSearchPhaseExecutionException($error): string
-  {
-    $message = collect();
-    $message->push("{$error['error']['type']}: {$error['error']['reason']}");
-
-    foreach ($error['error']['root_cause'] as $phase) {
-      if($phase['type'] == 'script_exception'){
-        $message->push("Caused By: {$phase['type']}");
-        $message->push(...$phase['script_stack']);
-      } else {
-        $message->push("Caused By: {$error['type']}");
-        $message->push("Index: {$phase['index']}");
-        $message->push("Reason: {$phase['reason']}");
-      }
+    /**
+     * Format the error message.
+     *
+     * Takes the first {$this->errorLimit} bulk issues and concatenates them to a single string message
+     */
+    private function formatMessage(Exception $result): string
+    {
+        // Clean that ish up.
+        return match (get_class($result)) {
+            MissingParameterException::class => $this->formatMissingParameterException($result),
+            ClientResponseException::class => $this->routeClientResponseException($result),
+        };
     }
 
-    return $message->implode(PHP_EOL);
-  }
+    private function formatMissingParameterException($result): string
+    {
+        return $result->getMessage();
+    }
 
-  private function formatParseException($error): string
-  {
-    $message = collect();
-    $message->push("{$error['error']['type']}: {$error['error']['reason']}");
-    return $message->implode(PHP_EOL);
-  }
+    private function routeClientResponseException($result): string
+    {
 
-  private function formatScriptException($error): string
-  {
-    $message = collect();
-    $message->push("{$error['error']['type']}: {$error['error']['reason']}");
-    $message->push("Caused By: {$error['error']['caused_by']['type']}");
-    $message->push(...$error['error']['script_stack']);
+        $error = json_decode((string) $result->getResponse()->getBody(), true);
 
-    return $message->implode(PHP_EOL);
-  }
+        return match ($error['error']['type']) {
+            'search_phase_execution_exception' => $this->formatSearchPhaseExecutionException($error),
+            'script_exception' => $this->formatScriptException($error),
+            default => $this->formatParseException($error),
+        };
+    }
 
+    private function formatSearchPhaseExecutionException($error): string
+    {
+        $message = collect();
+        $message->push("{$error['error']['type']}: {$error['error']['reason']}");
+
+        foreach ($error['error']['root_cause'] as $phase) {
+            if ($phase['type'] == 'script_exception') {
+                $message->push("Caused By: {$phase['type']}");
+                $message->push(...$phase['script_stack']);
+            } else {
+                $message->push("Caused By: {$error['error']['type']}");
+                if (! empty($phase['index'])) {
+                    $message->push("Index: {$phase['index']}");
+                }
+                $message->push("Reason: {$phase['reason']}");
+            }
+        }
+
+        return $message->implode(PHP_EOL);
+    }
+
+    private function formatParseException($error): string
+    {
+        $message = collect();
+        $message->push("{$error['error']['type']}: {$error['error']['reason']}");
+
+        return $message->implode(PHP_EOL);
+    }
+
+    private function formatScriptException($error): string
+    {
+        $message = collect();
+        $message->push("{$error['error']['type']}: {$error['error']['reason']}");
+        $message->push("Caused By: {$error['error']['caused_by']['type']}");
+        $message->push(...$error['error']['script_stack']);
+
+        return $message->implode(PHP_EOL);
+    }
 }
