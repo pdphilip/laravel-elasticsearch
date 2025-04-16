8bf1d86b771a2d252142343354b279f77a747235
David Philip <pd.philip@gmail.com>
2025-04-16 23:57:12 +0200

    Pint format

--- src/Relations/Traits/InteractsWithPivotTable.php
+++ src/Relations/Traits/InteractsWithPivotTable.php
@@ -1,38 +1,36 @@
 <?php
 
-  declare(strict_types=1);
+declare(strict_types=1);
 
-  namespace PDPhilip\Elasticsearch\Relations\Traits;
+namespace PDPhilip\Elasticsearch\Relations\Traits;
 
-  use BackedEnum;
+use BackedEnum;
 
-  trait InteractsWithPivotTable
-  {
-
+trait InteractsWithPivotTable
+{
     /** {@inheritdoc} */
     protected function formatRecordsList(array $records)
     {
-      return collect($records)->mapWithKeys(function ($attributes, $id) {
-        if (! is_array($attributes)) {
-          [$id, $attributes] = [$attributes, []];
-        }
+        return collect($records)->mapWithKeys(function ($attributes, $id) {
+            if (! is_array($attributes)) {
+                [$id, $attributes] = [$attributes, []];
+            }
 
-        if ($id instanceof BackedEnum) {
-          $id = $id->value;
-        }
+            if ($id instanceof BackedEnum) {
+                $id = $id->value;
+            }
 
-        // We have to convert all Key Ids to string values to keep it consistent in Elastic.
-        return [(string) $id => $attributes];
-      })->all();
+            // We have to convert all Key Ids to string values to keep it consistent in Elastic.
+            return [(string) $id => $attributes];
+        })->all();
     }
 
     /** {@inheritdoc} */
     protected function parseIds($value)
     {
-      // We have to convert all Key Ids to string values to keep it consistent in Elastic.
-      return collect(parent::parseIds($value))->mapWithKeys(function ($value, $key) {
-        return [(string) $key => $value];
-      })->all();
+        // We have to convert all Key Ids to string values to keep it consistent in Elastic.
+        return collect(parent::parseIds($value))->mapWithKeys(function ($value, $key) {
+            return [(string) $key => $value];
+        })->all();
     }
-
-  }
+}
